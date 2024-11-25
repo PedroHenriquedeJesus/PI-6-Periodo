ff65629c0dfcce40e6dc07cfa3bedf91d733cfc3
KaykySG <kaykyfsoares@hotmail.com>
2024-11-25 20:33:12 -0300

    iniciando Back

---
+++ Back-PI/src/test/java/com/projetoPI/VPK/BackPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoPI.VPK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
